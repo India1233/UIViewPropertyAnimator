633697623935b657a0a12f753481fc48d659201d
Shigas <India1233@users.noreply.github.com>
2019-12-17 11:49:57 +0530

    Initial Commit

---
+++ UIViewPropertyAnimator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIViewPropertyAnimator
+//
+//  Created by Suresh Shiga on 17/12/19.
+//  Copyright © 2019 Test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
