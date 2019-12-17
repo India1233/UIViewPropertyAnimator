4714e2fd7c73d9d67166ad26256ee713d8b9fa02
Shigas <India1233@users.noreply.github.com>
2019-12-17 16:25:21 +0530

    init

--- UIViewPropertyAnimator/ViewController.swift
+++ UIViewPropertyAnimator/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var planetImageView: UIImageView!
+    
+    let animator = UIViewPropertyAnimator(duration: 1.0, curve: .linear)
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        configureInitialSetup()
+        configureEndAnimations()
+       
     }
 
+    private func configureInitialSetup() {
+        planetImageView.alpha = 0.0
+        planetImageView.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
+    }
+    
+    private func configureEndAnimations() {
+        animator.addAnimations {
+            self.planetImageView.alpha = 1.0
+            self.planetImageView.transform = CGAffineTransform(scaleX: 3, y: 3)
+        }
+        
+        animator.addAnimations({
+            self.planetImageView.frame.origin.y -= 400
+        }, delayFactor: 0.3)
+    }
 
+    @IBAction func valueDidChanged(_ sender: UISlider) {
+        animator.fractionComplete = CGFloat(sender.value)
+    }
 }
+
+
 
